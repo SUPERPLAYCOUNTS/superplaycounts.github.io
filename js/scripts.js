9cef3fd04c5796bf500c7df2b08b164bd6d369d9
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2022-05-24 15:14:40 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,1 @@
-document.querySelector('.themes').addEventListener('change', (event) => {
-   if (event.target.nodeName === 'INPUT') {
-   	console.log('asdas');
-   	document.documentElement.classList.remove('dark', 'light');
-   	document.documentElement.classList.add(event.target.value);
-   }
-});
 
-let activeThere = localStorage.getItem('there');
-
-if(activeThere === null) {
-	applyThere('light');
-} else {
-	applyThere(activeThere);
-}
