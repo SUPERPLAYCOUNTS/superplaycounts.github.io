7d23d6337aa804563764cbe2fae194a2bedec8ee
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2022-05-28 14:20:34 +0300

    Update odometerfake.js

--- js/odometerfake.js
+++ js/odometerfake.js
@@ -1,1 +1,23 @@
-const channel = channelSubmitName.value; let count = 0; let rate = 0; function submit() { const channel = channelSubmitName.value; count = parseInt(countSubmit.value, 10); if (!channel) { return alert("Invalid channel name."); } if (typeof count === "undefined") { return alert("Count must be a number."); } if (count < -1e12 || count > 1e12) { return alert( "Count must be between -1 000 000 000 000 and 1 000 000 000 000." ); } channelSubs.innerHTML = count; channelName.innerHTML = channel; if (parseInt(subsPerMinute.value, 10)) { rate = parseInt(subsPerMinute.value, 10); } else { rate = 0; } if (rate > 1e9 || rate < -1e9) { return alert("Rate must be between -1 000 000 000 and 1 000 000 000."); } } function updateSubs() { if (rate > 1e9 || rate < -1e9) return; subsOdometer.innerHTML = Math.floor(count + rate / 80); count = count + rate / 80; } setInterval(updateSubs, 2000); 
+const channel = channelSubmitName.value; let count = 0; let rate = 0; function submit() { const channel = channelSubmitName.value; count = parseInt(countSubmit.value, 10); if (!channel) { return alert("Invalid channel name."); } if (typeof count === "undefined") { return alert("Count must be a number."); } if (count < -1e12 || count > 1e12) { return alert( "Count must be between -1 000 000 000 000 and 1 000 000 000 000." ); } channelSubs.innerHTML = count; channelName.innerHTML = channel; if (parseInt(subsPerMinute.value, 10)) { rate = parseInt(subsPerMinute.value, 10); } else { rate = 0; } if (rate > 1e9 || rate < -1e9) { return alert("Rate must be between -1 000 000 000 and 1 000 000 000."); } } function updateSubs() { if (rate > 1e9 || rate < -1e9) return; subsOdometer.innerHTML = Math.floor(count + rate / 80); count = count + rate / 80; } setInterval(updateSubs, 2000);
+let uploadbutton = document.getElementById("upload-button");
+let chosenImage = document.getElementById("chosen-image");
+let chosenBanner = document.getElementById("chosen-banner");
+let uploadbanner = document.getElementById("upload-banner");
+
+uploadbutton.onchange = () => {
+    let reader = new FileReader();
+    reader.readAsDataURL(uploadbutton.files[0]);
+    console.log(uploadbutton.files[0]);
+    reader.onload = () => {
+        chosenImage.setAttribute("src",reader.result);
+    }
+}
+
+uploadbanner.onchange = () => {
+    let reader = new FileReader();
+    reader.readAsDataURL(uploadbanner.files[0]);
+    console.log(uploadbanner.files[0]);
+    reader.onload = () => {
+        chosenBanner.setAttribute("src",reader.result);
+    }
+}
