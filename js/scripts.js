1dd4bc704489f4f0475103b1f26e9d0544e68e57
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2023-04-15 13:18:09 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let header__burger = document.querySelector('.header__burger');
 let header__menu = document.querySelector('.header__menu');
 
+
 header__burger.addEventListener('click', function(){
 	header__burger.classList.toggle('active');
 	header__menu.classList.toggle('active');
@@ -14,4 +15,60 @@ const updateYear = () => {
 	}
 }
 
-setInterval(updateYear, 2);
+setInterval(updateYear, 15);
+
+function setTheme(theme) {
+    var moonIcon = document.getElementById("moon-icon");
+    var sunIcon = document.getElementById("sun-icon");
+    var link = document.getElementById("theme-stylesheet");
+    var chartBackgroundColor = theme === 'dark' ? '#161616' : '#fff';
+    
+    if (theme === "dark") {
+        moonIcon.style.display = "none";
+        sunIcon.style.display = "block";
+        link.href = "/css/dark.css";
+        localStorage.setItem('theme', 'dark');
+    } else {
+        moonIcon.style.display = "block";
+        sunIcon.style.display = "none";
+        link.href = "/css/light.css";
+        localStorage.setItem('theme', 'light');
+    }
+
+    Highcharts.chart('container', {
+		chart: {
+		  type: 'line',
+		  backgroundColor: chartBackgroundColor
+		},
+		title: {
+		  text: ''
+		},
+		xAxis: {
+		  type: 'datetime'
+		},
+		yAxis: {
+		  title: {
+			text: ''
+		  },
+		  type: 'linear'
+		},
+		credits: {
+		  text: 'superplaycounts.github.io',
+		  href: 'https://superplaycounts.github.io/',
+		},
+		series: [{
+		  name: 'Subscribers',
+		  data: apiData,
+		  color: 'red'
+		}]
+	  }, function (ch) {
+		chart = ch;
+	  });
+}
+  
+  let theme = localStorage.getItem('theme');
+  if (theme === 'dark') {
+	setTheme('dark');
+  } else {
+	setTheme('light');
+  }
